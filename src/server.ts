42f1e94d0272a5297ddf8185543b51c4c61e166d
mujeebminhas88-lab <mujeebminhas88@gmail.com>
2026-07-09 20:30:17 +0500

    Delete src/server.ts

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-// server.ts
-import handler, { createServerEntry } from "@tanstack/react-start/server-entry";
-
-export default createServerEntry({
-  fetch(request) {
-    return handler.fetch(request);
-  },
-});
